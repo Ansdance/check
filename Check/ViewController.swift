4f807bab7d6e895a10c0bc1bbbcb25a7f9573c5f
ansardauletbayev <dauletbayev.ansar@gmail.com>
2023-07-07 18:20:44 +0600

    добавил параметр

--- Check/ViewController.swift
+++ Check/ViewController.swift
@@ -8,9 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let D = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
